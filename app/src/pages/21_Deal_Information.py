8e7690a8b4dffad4183b15ed19a0040487be51a6
franipendus <pendus.f@northeastern.edu>
2024-06-05 21:57:37 +0200

    added deal info buttons

--- app/src/pages/21_Deal_Information.py
+++ app/src/pages/21_Deal_Information.py
@@ -8,14 +8,18 @@ SideBarLinks()
 
 st.title('Deal Information')
 
+st.title('Ad Information')
+if st.button('Post a Deal'):
+    st.switch_page('pages/23_Post_Deal.py')
+
+if st.button('Delete an Deal'):
+    st.switch_page('pages/24_Delete_Deal.py')
+
 st.write("### Your deals:")
 num = st.number_input('Deal Administrator ID', min_value=1, max_value=50, value= 1,                  
                     label_visibility="visible")
 results = requests.get(f'http://api:4000/d/dealadmin/deals/{num}').json()
 st.table(results)
-
-if st.button('Post an Deal'):
-    st.write('finish this')
 
 
 st.write("### Want the deal information about a specific hotel?")
