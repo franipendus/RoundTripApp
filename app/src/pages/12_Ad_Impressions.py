71cc2abe1ddd8d177d266d7a8ac60047fc1d1a91
franipendus <pendus.f@northeastern.edu>
2024-06-05 18:18:20 +0200

    changed ad imp wording

--- app/src/pages/12_Ad_Impressions.py
+++ app/src/pages/12_Ad_Impressions.py
@@ -7,13 +7,13 @@ SideBarLinks()
 
 st.title('Ad Impressions')
 
-st.write("### Your ads:")
+st.write("### Select an ad to view impressions:")
 num = st.number_input('Ad ID', min_value=1, max_value=10, value= 1,                  
                     label_visibility="visible")
 results = requests.get(f'http://api:4000/a/advertisers/adimp/{num}').json()
 st.table(results)
 
-st.write("### Want the information about a specific traveler's impressions?")
+st.write("### Want the information about the impressions from a specific traveler?")
 num = st.number_input('Traveler ID', min_value=1, max_value=50, value= 1,                  
                     label_visibility="visible")
 results = requests.get(f'http://api:4000/a/advertisers/adimp/trav/{num}').json()
