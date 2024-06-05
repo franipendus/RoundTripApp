3f5bb6dc519b33b6848deb0cbd6fbe2792d99bb4
erica-iii <indman.e@northeastern.edu>
2024-06-05 16:57:06 +0200

    added predictions

---
+++ api/backend/ml_models/model01.py
@@ -0,0 +1,97 @@
+"""
+model01.py is an example of how to access model parameter values that you are storing
+in the database and use them to make a prediction when a route associated with prediction is
+accessed. 
+"""
+from backend.db_connection import db
+import numpy as np
+import logging
+
+
+def train():
+  """
+  You could have a function that performs training from scratch as well as testing (see below).
+  It could be activated from a route for an "administrator role" or something similar. 
+  """
+  return 'Training the model'
+
+def test():
+  return 'Testing the model'
+
+def predict(var01, var02, var03, var04, var05):
+  """
+  Retreives model parameters from the database and uses them for real-time prediction
+  """
+  # get a database cursor 
+  cursor = db.get_db().cursor()
+  # get the model params from the database
+  query = 'SELECT beta_vals FROM model1_params ORDER BY sequence_number DESC LIMIT 1'
+  cursor.execute(query)
+  return_val = cursor.fetchone()
+
+  params = return_val['beta_vals']
+  logging.info(f'params = {params}')
+  logging.info(f'params datatype = {type(params)}')
+
+  # turn the values from the database into a numpy array
+  params_array = np.array(list(map(float, params[1:-1].split(','))))
+  logging.info(f'params array = {params_array}')
+  logging.info(f'params_array datatype = {type(params_array)}')
+
+  #scale var 1 and 2, convert var 3 and 4 into
+  var01 = (var01 - 323.81361111111113)/64.21462998123202
+  var02 = (var02 - 4.762450341836946)/0.08312695791275146
+
+  if var03 == 'London':
+    co = np.array([0,0,0])
+  if var03 == 'Madrid':
+    co = np.array([1,0,0])
+  if var03 == 'Paris':
+    co = np.array([0,1,0])
+  if var03 == 'Rome':
+    co = np.array([0,0,1])
+
+  if var04 == 'London':
+    gdp = 57978.2
+    cd = np.array([0,0,0])
+  if var04 == 'Madrid':
+    gdp = 53710.2
+    cd = np.array([1,0,0])
+  if var04 == 'Paris':
+    gdp = 60035.2
+    cd = np.array([0,1,0])
+  if var04 == 'Rome':
+    gdp = 58631.8
+    cd = np.array([0,0,1])
+  
+  if var05 in ('January', 'February', 'March'):
+    quarter = np.array([0,0,0])
+  if var05 in ('April', 'May', 'June'):
+    quarter = np.array([1,0,0])
+  if var05 in ('July', 'August', 'September'):
+    quarter = np.array([0,1,0])
+  if var05 in ('October', 'November', 'December'):
+    quarter = np.array([0,0,1])
+
+  # turn the variables sent from the UI into a numpy array
+  # update this
+  numerical_values = np.array([1.0, float(var01), float(var02), float(gdp)])
+  input_array = np.concatenate((numerical_values, co, cd, quarter))
+  #input_array = np.array([1.0, float(var01), float(var02), float(gdp), co, cd, quarter])
+  
+  # calculate the dot product (since this is a fake regression)
+  prediction = np.dot(params_array, input_array)
+
+  return prediction
+
+  ##############################################################
+  
+  # # retreive the parameters from the appropriate table
+  # cursor.execute('select beta_0, beta_1, beta_2 from model1_param_vals')
+  # # fetch the first row from the cursor
+  # data = cursor.fetchone()
+  # # calculate the predicted result using this functions arguments as well as the model parameter values
+  # result = data[0] + int(var01) * data[1] + int(var02) * data[2]
+
+  # # return the result 
+  # return result
--- app/src/pages/04_Prediction.py
+++ app/src/pages/04_Prediction.py
@@ -10,20 +10,34 @@ st.set_page_config(layout = 'wide')
 # Display the appropriate sidebar links for the role of the logged in user
 SideBarLinks()
 
-st.title('Prediction with Regression')
+st.title('Cost Predictions')
+st.write('### Prediction with Regression')
 
 # create a 2 column layout
-col1, col2 = st.columns(2)
+col1, col2, col3, col4, col5 = st.columns(5)
 
-# add one number input for variable 1 into column 1
+# add one number input for variable 1 into column 1 flight price
 with col1:
-  var_01 = st.number_input('Variable 01:',
+  var_01 = st.number_input('Flight Price:',
                            step=1)
 
-# add another number input for variable 2 into column 2
+# add another number input for variable 2 into column 2 hotel rating
 with col2:
-  var_02 = st.number_input('Variable 02:',
+  var_02 = st.number_input('Hotel Rating:',
                            step=1)
+  
+# add another number input for variable 3 into column 3 origin city
+with col3:
+  var_03 = st.selectbox('Origin City:', ('London', 'Madrid', 'Rome', 'Paris'))
+  
+# add another number input for variable 4 into column 4 destination city
+with col4:
+  var_04 = st.selectbox('Destination City:', ('London', 'Madrid', 'Rome', 'Paris'))
+
+# add another number input for variable 5 into column 5 month
+with col5:
+  var_05 = st.selectbox('In which month do you want to travel?:', ('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'))
+
 
 logger.info(f'var_01 = {var_01}')
 logger.info(f'var_02 = {var_02}')
@@ -33,6 +47,9 @@ logger.info(f'var_02 = {var_02}')
 if st.button('Calculate Prediction',
              type='primary',
              use_container_width=True):
-  results = requests.get(f'http://api:4000/c/prediction/{var_01}/{var_02}').json()
+  results = requests.get(f'http://api:4000/c/prediction/{var_01}/{var_02}/{var_03}/{var_04}/{var_05}').json()
   st.dataframe(results)
+
+
+
   
